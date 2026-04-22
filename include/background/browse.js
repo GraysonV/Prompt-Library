4931472383e68b76cdd23bc0e8025ea70e09fbbe
Grayson <127267127+GraysonV@users.noreply.github.com>
2026-04-21 19:21:20 -0500

    Filter by Subject, took WAY too long to figure out
    Firebase, more like

--- include/background/browse.js
+++ include/background/browse.js
@@ -1,13 +1,31 @@
 const container = document.getElementById('prompts-display');
+const subjectSelect = document.getElementById('input-subject');
+const orderSelect = document.getElementById('input-order');
 // const buttonPrompts = document.getElementById('button-prompts');
-const SCROLL_BOTTOM_OFFSET = -50
+const SCROLL_BOTTOM_OFFSET = -50;
 const limit = 10;
+const limitLarge = 30;
 let startAfter = "";
 let finished = false;
 let scrollPaused = true;
 
+let params = new URLSearchParams(document.location.search);
+let searchQuery = params.get("q"); // Is the search query
+let subjectQuery = params.get("s"); // Is the subject query
+let orderQuery = params.get("o"); // Asc/Desc query
+if (subjectQuery != null) {
+    subjectSelect.value = subjectQuery;
+}
+if (orderQuery == null) {
+    orderQuery = "desc";
+} else {
+    orderSelect.value = orderQuery;
+}
+console.log(orderQuery);
+
 // Load prompts, returns true or false if it loads them.
 async function loadPrompts() {
+    
     let newContainer = document.createElement("div");
     newContainer.innerHTML = '<p>Loading...</p>';
     container.appendChild(newContainer);
@@ -15,17 +33,27 @@ async function loadPrompts() {
     scrollPaused = true;
 
     try {
-        const snapshot = await db.collection('prompts').orderBy('timestamp', 'desc').startAfter(startAfter).limit(limit).get();
+        let snapshot;
+        let isSubjectQuery = (subjectQuery != null && subjectQuery != "all")
+
+        if (isSubjectQuery) {
+            snapshot = 
+                await db.collection('prompts')
+                .where('subject', "==", subjectQuery)
+                .limit(limitLarge)
+                .get();
+        } else {
+            // TODO: Replace "timestamp" string with a number that actually represents the time
+            // it was uploaded so I can order it properly.
+            snapshot = 
+                await db.collection('prompts')
+                .orderBy('timestamp', 'desc')
+                .startAfter(startAfter)
+                .limit(limit)
+                .get();
+        }
         newContainer.innerHTML = '';
         // buttonPrompts.hidden = true;
-
-        if (snapshot.empty || snapshot.size < limit) {
-            newContainer.innerHTML = '<hr><p>No more prompts. How sad!</p> <p><small><a href="#">Top of the Page &uArr;</a></small></p>';
-            // Button Prompt stays hidden
-            return false;
-        } else {
-            // buttonPrompts.hidden = false;
-        }
 
         snapshot.forEach(doc => {
             const data = doc.data();
@@ -34,6 +62,14 @@ async function loadPrompts() {
             // which is then used for loading the next set of prompts.
             startAfter = data.timestamp;
         });
+
+        if (snapshot.empty || snapshot.size < limit || isSubjectQuery) {
+            newContainer.innerHTML += '<hr><p>No more prompts. How sad!</p> <p><small><a href="#">Top of the Page &uArr;</a></small></p>';
+            // Button Prompt stays hidden
+            return false;
+        } else {
+            // buttonPrompts.hidden = false;
+        }
     } catch (e) {
         newContainer.innerHTML = 'Error loading. Check Firestore.';
         return false;
@@ -61,9 +97,45 @@ finished != loadPrompts();
 
 addEventListener("scroll", function() {
     if (!finished && !scrollPaused) {
-        console.log(pageYOffset + window.innerHeight >= document.body.scrollHeight + SCROLL_BOTTOM_OFFSET);
+        // console.log(pageYOffset + window.innerHeight >= document.body.scrollHeight + SCROLL_BOTTOM_OFFSET);
         if (pageYOffset + window.innerHeight >= document.body.scrollHeight + SCROLL_BOTTOM_OFFSET) {
             finished != loadPrompts();
         }
     }
 });
+
+subjectSelect.onchange = (event) => {
+    var inputText = event.target.value;
+
+    let url = window.location.href.split("?")[0];
+    url += '?s=' + inputText;
+
+    if (searchQuery != null){
+        url += '&q=' + searchQuery;
+    }
+    if (orderQuery != null){
+        url += '&o=' + orderQuery;
+    }
+
+    window.location.href = url;
+
+    console.log(inputText);
+}
+
+orderSelect.onchange = (event) => {
+    var inputText = event.target.value;
+
+    let url = window.location.href.split("?")[0];
+    url += '?o=' + inputText;
+
+    if (searchQuery != null){
+        url += '&q=' + searchQuery;
+    }
+    if (subjectQuery != null){
+        url += '&s=' + subjectQuery;
+    }
+
+    window.location.href = url;
+
+    console.log(inputText);
+}
